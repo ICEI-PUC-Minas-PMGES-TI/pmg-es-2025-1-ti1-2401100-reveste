1a1a4440c90e3ef4d5d0c49ee6af4f0e7353bfe4
Rafael Lopes de Souza da Fonseca <rafaellopessouzafonseca@gmail.com>
2025-05-10 10:27:57 -0300

    Colocando máscaras no formulário

--- codigo/Rafael_Lopes/js/app.js
+++ codigo/Rafael_Lopes/js/app.js
@@ -1,16 +1,51 @@
 document.getElementById('cadastroForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede o envio padrão do formulário
-  
-    const formData = new FormData(event.target);
-  
-    const dados = {
-      nome: formData.get('nome'),
-      email: formData.get('email'),
-      senha: formData.get('senha'),
-      cnpj: formData.get('cnpj'),
-      telefone: formData.get('telefone')
-    };
-  
-    console.log("JSON de cadastro:", JSON.stringify(dados));
-  });
-  
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  const dados = {
+    nome: formData.get('nome'),
+    email: formData.get('email'),
+    senha: formData.get('senha'),
+    cnpj: formData.get('cnpj'),
+    telefone: formData.get('telefone')
+  };
+
+  console.log("JSON de cadastro:", JSON.stringify(dados));
+});
+
+const cnpjInput = document.querySelector('#cnpj');
+
+cnpjInput.addEventListener('input', () => {
+  let value = cnpjInput.value.replace(/\D/g, '');
+
+  if (value.length > 14) value = value.slice(0, 14);
+
+  value = value
+    .replace(/^(\d{2})(\d)/, '$1.$2')
+    .replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3')
+    .replace(/\.(\d{3})(\d)/, '.$1/$2')
+    .replace(/(\d{4})(\d)/, '$1-$2');
+
+  cnpjInput.value = value;
+});
+
+const telefoneInput = document.querySelector('#telefone');
+
+telefoneInput.addEventListener('input', () => {
+  let value = telefoneInput.value.replace(/\D/g, ''); 
+
+  if (value.length > 11) value = value.slice(0, 11);
+
+  if (value.length <= 10) {
+    value = value
+      .replace(/^(\d{2})(\d)/, '($1) $2')
+      .replace(/(\d{4})(\d)/, '$1-$2');
+  } else {
+    value = value
+      .replace(/^(\d{2})(\d)/, '($1) $2')
+      .replace(/(\d{5})(\d)/, '$1-$2');
+  }
+
+  telefoneInput.value = value;
+});
