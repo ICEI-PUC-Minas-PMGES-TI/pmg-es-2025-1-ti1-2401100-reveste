5b4c0483505276b7a5e90ee0f862b6b622c2f54a
Oieusouopi <rafasc866@gmail.com>
2025-05-12 20:19:57 -0300

    getLocaltion

--- codigo/Rafael_Santos/js/index.js
+++ codigo/Rafael_Santos/js/index.js
@@ -2,15 +2,16 @@ import Apoio from "./model/Apoio.js";
 
 function onInit() {
     fetch('./utils/dados.json')
-    .then(response => response.json())
-    .then(dadosResponse => {
-      let dados = dadosResponse;
-      for (let dado of dados) {
-        setCards(dado);
-      }
-    })
-    .catch(error => console.error('Erro ao carregar os dados:', error));
+        .then(response => response.json())
+        .then(dadosResponse => {
+            let dados = dadosResponse;
+            for (let dado of dados) {
+                setCards(dado);
+            }
+        })
+        .catch(error => console.error('Erro ao carregar os dados:', error));
 
+    getLocation();
 }
 
 
@@ -40,12 +41,51 @@ function setCards(card) {
     container.style.width = "191px";
 
     title.style.fontSize = "large"
-    
+
     img.style.width = "156px";
     img.style.height = "65px";
 
     address.style.fontSize = "x-small";
 }
+
+// function getLocation() {
+
+//            if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(
+//       async (position) => {
+//         const userLocation = {
+//           lat: position.coords.latitude,
+//           lng: position.coords.longitude,
+//         };
+
+//         const gmpMapElement = document.querySelector('gmp-map');
+//         if (!gmpMapElement) {
+//           console.error('gmp-map não encontrado!');
+//           return;
+//         }
+
+//         // Espera o mapa carregar (caso ainda não tenha carregado)
+//         await gmpMapElement.ready;
+
+//         // Cria o marcador com a nova API
+//         const marker = new google.maps.marker.AdvancedMarkerElement({
+//           map: gmpMapElement,
+//           position: userLocation,
+//           title: 'Sua Localização',
+//         });
+
+//         // Centraliza e aplica o zoom no mapa
+//         gmpMapElement.zoom = 15;
+//         gmpMapElement.center = userLocation;
+//       },
+//       () => {
+//         alert("Erro ao obter sua localização.");
+//       }
+//     );
+//   } else {
+//     alert("Geolocalização não é suportada por este navegador.");
+//   }
+// }
 
 
 document.addEventListener('DOMContentLoaded', function () {
