b1789294b950a695e96ec63aecb9564628951f93
Bruno Hoske <brunohoske@gmail.com>
2025-05-11 23:53:24 -0300

    Correção cadastral no json de dados e no formulario

--- codigo/BrunoAguiar/js/app.js
+++ codigo/BrunoAguiar/js/app.js
@@ -1,9 +1,9 @@
-const model = { 
+const agendamento = { 
     id: 2, 
     idPontoApoio: 1, 
     nome: "João Silva", 
     email: "joao.silva@example.com", 
-    data_nascimento: "1990-05-15", 
+    cpf: "15221451678", 
     data_horario_doacao: "2025-05-10T14:30:00" 
   };
   
