3e8e4cd63f27d3d346cb830c4e38802387b011b7
Murilo Freitas de Souza <onemurilo@gmail.com>
2025-05-12 13:49:34 -0300

    Corrige arquivos e unifica commits

--- codigo/Murilo/js/app.js
+++ codigo/Murilo/js/app.js
@@ -1,28 +1,12 @@
-var rangeInput = document.getElementById("distancia");
-var rangeText = document.getElementById("rangeText");
 
-rangeText.innerText = rangeInput.value + " km";
-
-rangeInput.addEventListener("input", function () {
-    rangeText.innerText = rangeInput.value + " km";
-});
-
-function salvarFiltro() {
-    let selectRoupas = document.getElementById("peca");
-    let favoritosButao = document.getElementById("favoritos");
-
-    const selectRoupasValor = selectRoupas.value;
-    const rangeInputFiltro = rangeInput.value;
-    const favoritosButaoValor = favoritosButao.checked;
-
-    console.log("Roupas:", selectRoupasValor);
-    console.log("Distância:", rangeInputFiltro);
-    console.log("Favoritos:", favoritosButaoValor);
-}
+//Carrega as imagens ao carregar a página
 window.addEventListener('DOMContentLoaded', () =>{
     let container = document.getElementById("images-container");
     
     let imagesUrls = [
+    "/img/62a8c2db1865d.jpg",
+    "/img/62a8c2db1865d.jpg",
+    "/img/62a8c2db1865d.jpg",
     "/img/62a8c2db1865d.jpg",
     "/img/62a8c2db1865d.jpg",
     "/img/62a8c2db1865d.jpg",
@@ -39,15 +23,40 @@ window.addEventListener('DOMContentLoaded', () =>{
         img.alt = "Pontos";
         img.style.width = "200px";
         img.style.margin = "10px";
+        img.style.borderRadius = '5px';
         container.appendChild(img);
     })
 
 })
 
+//Range Button
+var rangeInput = document.getElementById("distancia");
+var rangeText = document.getElementById("rangeText");
+
+rangeText.innerText = rangeInput.value + " km";
+rangeText.setAttribute('class', 'bg-light p-2 mt-4 rounded-1');
+
+rangeInput.addEventListener("input", function () {
+    rangeText.innerText = rangeInput.value + " km";
+});
+
+function aplicarFiltro() {
+    let selectRoupas = document.getElementById("peca");
+    let favoritosButao = document.getElementById("favoritos");
+
+    const selectRoupasValor = selectRoupas.value;
+    const rangeInputFiltro = rangeInput.value;
+    const favoritosButaoValor = favoritosButao.checked;
+
+    console.log("Roupas:", selectRoupasValor);
+    console.log("Distância:", rangeInputFiltro);
+    console.log("Favoritos:", favoritosButaoValor);
+}
 
 
-var butaoSalvarFiltro = document.getElementById("butaoSalvarFiltro");
-butaoSalvarFiltro.addEventListener("click", salvarFiltro);
+
+var butaoAplicarFiltro = document.getElementById("butaoAplicarFiltro");
+butaoAplicarFiltro.addEventListener("click", aplicarFiltro);
 
 //Aplicar filtro
 document.querySelector(".button-filter").addEventListener("click", function () {
@@ -57,7 +66,7 @@ document.querySelector(".button-filter").addEventListener("click", function () {
     let images = document.createElement('img');
     images.src = '/img/62a8c2db1865d.jpg'
     images.style.width = '200px';
-    images.style
+    images.style.borderRadius = '5px';
     containerImages.appendChild(images);
 
 
