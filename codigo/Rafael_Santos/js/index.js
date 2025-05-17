0fff6c88308286e8c9f2945b4f14bc5a09362379
Oieusouopi <rafasc866@gmail.com>
2025-05-17 16:10:57 -0300

    mostra a localizacao e clicar vai ate a localizacao

--- codigo/Rafael_Santos/js/index.js
+++ codigo/Rafael_Santos/js/index.js
@@ -7,11 +7,10 @@ function onInit() {
             let dados = dadosResponse;
             for (let dado of dados) {
                 setCards(dado);
+                setAdvancedMapMarker(dado);
             }
         })
         .catch(error => console.error('Erro ao carregar os dados:', error));
-
-    getLocation();
 }
 
 
@@ -40,52 +39,29 @@ function setCards(card) {
     container.classList.add("card");
     container.style.width = "191px";
 
-    title.style.fontSize = "large"
+    title.style.fontSize = "large";
+    title.style.whiteSpace = "nowrap";
 
     img.style.width = "156px";
     img.style.height = "65px";
 
     address.style.fontSize = "x-small";
+
+    container.addEventListener('click', () => {
+        const map = document.getElementById('map');
+        map.setAttribute('center', `${apoio.local.x},${apoio.local.y}`);
+    });
 }
 
-// function getLocation() {
+function setAdvancedMapMarker(card) {
+    const apoio = new Apoio(card);
+    const map = document.getElementById('map');
 
-//            if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(
-//       async (position) => {
-//         const userLocation = {
-//           lat: position.coords.latitude,
-//           lng: position.coords.longitude,
-//         };
+    const gMapAdvancedMarker = document.createElement('gmp-advanced-marker');
+    gMapAdvancedMarker.setAttribute('position', `${apoio.local.x},${apoio.local.y}`);
+    map.appendChild(gMapAdvancedMarker);
+}
 
-//         const gmpMapElement = document.querySelector('gmp-map');
-//         if (!gmpMapElement) {
-//           console.error('gmp-map não encontrado!');
-//           return;
-//         }
-
-//         // Espera o mapa carregar (caso ainda não tenha carregado)
-//         await gmpMapElement.ready;
-
-//         // Cria o marcador com a nova API
-//         const marker = new google.maps.marker.AdvancedMarkerElement({
-//           map: gmpMapElement,
-//           position: userLocation,
-//           title: 'Sua Localização',
-//         });
-
-//         // Centraliza e aplica o zoom no mapa
-//         gmpMapElement.zoom = 15;
-//         gmpMapElement.center = userLocation;
-//       },
-//       () => {
-//         alert("Erro ao obter sua localização.");
-//       }
-//     );
-//   } else {
-//     alert("Geolocalização não é suportada por este navegador.");
-//   }
-// }
 
 
 document.addEventListener('DOMContentLoaded', function () {
