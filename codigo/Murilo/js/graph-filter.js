604327c2daec9c8e2df1c4e70f568c652fdf9759
Murilo Freitas de Souza <onemurilo@gmail.com>
2025-05-26 15:48:05 -0300

    UPDATE Animações do menu

--- codigo/Murilo/js/graph-filter.js
+++ codigo/Murilo/js/graph-filter.js
@@ -5,25 +5,17 @@ let aside = document.querySelector("#filter");
 
 function openFilterMenu() {
     aside.classList.add('active');
-    aside.style.display = 'flex';
-    aside.style.width = '20%';
-
     listMenuImg.src = './assets/img/close.png';
-    listMenuImg.style.width = '25px';
-    listMenuImg.style.height = '25px';
-    listMenuImg.classList.add('active');
+    listMenuImg.style.width = '30px';
+    listMenuImg.style.height = '30px';
 }
 
 function closeFilterMenu() {
-    aside.style.display = 'none';
     aside.classList.remove('active');
-
     listMenuImg.src = './assets/img/menu.png';
     listMenuImg.style.width = '40px';
     listMenuImg.style.height = '40px';
-    listMenuImg.classList.remove('active');
 }
-
 filterAppearBtn.addEventListener("click", ()=> {
     if (aside.classList.contains('active')){
         closeFilterMenu();
