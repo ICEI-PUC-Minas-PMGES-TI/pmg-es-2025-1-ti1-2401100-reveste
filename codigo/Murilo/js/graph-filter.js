f71b4cb60e5ff26321fc0680b53b0850fe67b7e6
Murilo Freitas de Souza <onemurilo@gmail.com>
2025-05-28 00:34:06 -0300

    Inserção do select para alterar o tipo do gráfico + melhorias na semântica do aside dinâmico

--- codigo/Murilo/js/graph-filter.js
+++ codigo/Murilo/js/graph-filter.js
@@ -1,23 +1,25 @@
 const filterAppearBtn = document.querySelector("#filter-appear-btn");
 const listMenuImg = document.querySelector('#list-menu-img');
-let aside = document.querySelector("#filter");
+const filterMenu = document.querySelector("#menu");
+const graphType = document.querySelector("#graph-type-select");
 
 
 function openFilterMenu() {
-    aside.classList.add('active');
+    filterMenu.classList.add('active');
     listMenuImg.src = './assets/img/close.png';
     listMenuImg.style.width = '30px';
     listMenuImg.style.height = '30px';
 }
-
 function closeFilterMenu() {
-    aside.classList.remove('active');
+    filterMenu.classList.remove('active');
     listMenuImg.src = './assets/img/menu.png';
     listMenuImg.style.width = '40px';
     listMenuImg.style.height = '40px';
 }
+
+
 filterAppearBtn.addEventListener("click", ()=> {
-    if (aside.classList.contains('active')){
+    if (filterMenu.classList.contains('active')){
         closeFilterMenu();
     }else{
         openFilterMenu();
